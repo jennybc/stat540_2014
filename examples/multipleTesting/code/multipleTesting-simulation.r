ab99bd8c44f36aaf572c84b6e6b2477729fabc2c
jennybc <jenny@stat.ubc.ca>
2014-02-06 21:26:57 -0800

    Refreshed the simulation used in the multiple testing lecture

--- examples/multipleTesting/code/multipleTesting-simulation.r
+++ examples/multipleTesting/code/multipleTesting-simulation.r
@@ -1,5 +1,6 @@
 library(qvalue)
-library(lattice)
+library(ggplot2)
+library(plyr)
 
 nStats <- 10000
 
@@ -7,127 +8,116 @@ set.seed(111503)
 
 pi0 <- 0.85                             # proportion of truly null
 
-jDat <- data.frame(yNull = rnorm(nStats),
-                   yExciting = rnorm(nStats, mean = 0.75),
-                   stateOfNature = rbinom(n = nStats, size = 1, prob = pi0))
-jDat$y <- with(jDat, ifelse(stateOfNature, yNull, yExciting))
+jDat <- data.frame(tNull = rnorm(nStats),
+                   tExciting = rnorm(nStats, mean = 0.75),
+                   isNull = rbinom(n = nStats, size = 1, prob = pi0))
+jDat$tObs <- with(jDat, ifelse(isNull, tNull, tExciting))
+table(jDat$isNull)
+#    0    1 
+# 1521 8479 
 
 head(jDat)
 
-densityplot(~ yNull + yExciting + y, jDat,
-            allow.multiple = TRUE, auto.key = TRUE,
-            type = c("p", "r", "g"))
+jDatTall <- stack(jDat, select = c(tNull, tObs, tExciting))
+names(jDatTall) <- c("testStat", "source")
+jDatTall$source <-
+  factor(jDatTall$source, levels = c("tNull", "tObs", "tExciting"))
+str(jDatTall)
 
-# dev.print(pdf,
-#           jPaste(whereAmI, "figs/densityplot-variousPvalues.pdf"),
-#           width = 8, height = 8)
+p <- ggplot(jDatTall, aes(x = testStat))
 
-foo <- lapply(jDat[c("yNull", "yExciting", "y")],
-              function(z) {
-                return(cbind(pnorm(z, lower.tail = FALSE),
-                             pnorm(abs(z), lower.tail = FALSE) * 2))})
-foo <- do.call(cbind, foo)
-colnames(foo) <- paste0(c("rtProbs", "tsProbs"),
-                        rep(c("Null", "Exc", "Obs"), each = 2))
+p + geom_density(aes(col = source)) +
+  geom_point(aes(x = testStat,
+                 y = rep(c(0.00, -0.035, -0.07), each = nStats),
+                 col = source), alpha = I(1/60))
 
-jDat <- data.frame(jDat, foo)
+dev.print(pdf,
+          "../figs/densityplot-various-test-stats.pdf",
+          width = 8, height = 8)
 
-histogram(~ rtProbsNull + tsProbsNull, jDat,
-          endpoints = c(0, 1), allow.multiple = TRUE, outer = TRUE,
-          xlab = "p-values for null data")
+jDatTall$oneSided <- pnorm(jDatTall$testStat, lower.tail = FALSE)
+jDatTall$twoSided <- pnorm(abs(jDatTall$testStat), lower.tail = FALSE) * 2
 
-histogram(~ rtProbsExc + tsProbsExc, jDat,
-          endpoints = c(0, 1), allow.multiple = TRUE, outer = TRUE,
-          xlab = "p-values for exciting data!")
+pDat <- data.frame(source = factor(jDatTall$source,
+                                   levels = levels(jDatTall$source)),
+                   isNull = jDat$isNull,
+                   stack(jDatTall, select = c(oneSided, twoSided)))
+str(pDat)
+names(pDat) <- c("source", "isNull", "pVal", "test")
+str(pDat)
 
-histogram(~ rtProbsObs + tsProbsObs, jDat,
-          endpoints = c(0, 1), allow.multiple = TRUE, outer = TRUE,
-          xlab =
-          'p-values for realistic data (mostly null + bit of exciting)')
+p <- ggplot(pDat, aes(x = pVal)) + xlab("p-value")
 
-histogram(~ rtProbsNull + tsProbsNull +
-          rtProbsExc + tsProbsExc +
-          rtProbsObs + tsProbsObs, jDat,
-          endpoints = c(0, 1), allow.multiple = TRUE, outer = TRUE,
-          xlab = "p-values", layout = c(2, 3),
-          scale = "free")
+p + geom_histogram(binwidth = 1/20, fill = I("grey40")) +
+  facet_grid(test ~ source) + aes(y = ..density..)
 
-# dev.print(pdf,
-#           jPaste(whereAmI, "figs/histogram-variousPvalues.pdf"),
-#           width = 8, height = 6.5)
+dev.print(pdf,
+          "../figs/histogram-variousPvalues.pdf",
+          width = 8, height = 6.5)
 
+ksRes <-
+  aggregate(pVal ~ source * test, pDat, function(x) ks.test(x, y = "punif")$p)
 
-ks.test(x = jDat$rtProbsNull, y = "punif")
-ks.test(x = jDat$rtProbsExc, y = "punif")
-ks.test(x = jDat$rtProbsObs, y = "punif")
+with(ksRes,
+     data.frame(source, test, ks.pval = round(pVal, 8)))
+#      source     test    ks.pval
+# 1     tNull oneSided 0.80181209
+# 2      tObs oneSided 0.00000000
+# 3 tExciting oneSided 0.00000000
+# 4     tNull twoSided 0.83212279
+# 5      tObs twoSided 0.00283168
+# 6 tExciting twoSided 0.00000000
 
-ks.test(x = jDat$tsProbsNull, y = "punif")
-ks.test(x = jDat$tsProbsExc, y = "punif")
-ks.test(x = jDat$tsProbsObs, y = "punif")
+## get q-values
+qVals <- dlply(pDat, ~ test * source, function(x) qvalue(x$pVal))
+str(qVals, max.level = 0)
+names(qVals)
 
-foo <- qvalue(jDat$rtProbsNull)
-foo$pi0
-plot(foo)
+## extract pi0
+cbind(round(sapply(qVals, "[[", "pi0"), 2))
+# oneSided.tNull     0.96
+# oneSided.tObs      0.85
+# oneSided.tExciting 0.20
+# twoSided.tNull     1.00
+# twoSided.tObs      0.98
+# twoSided.tExciting 0.79
 
-# dev.print(pdf,
-#           jPaste(whereAmI, "figs/qvaluePlot-rtProbsNull.pdf"),
-#           width = 8, height = 8)
+pDat$qVal <- unlist(lapply(qVals, "[[", "qvalues"))
 
-foo <- qvalue(jDat$rtProbsExc)
-foo$pi0
-plot(foo)
+p <- ggplot(pDat, aes(x = pVal, y = qVal))
 
-# dev.print(pdf,
-#           jPaste(whereAmI, "figs/qvaluePlot-rtProbsExc.pdf"),
-#           width = 8, height = 8)
+p + geom_point() + facet_grid(test ~ source) + coord_equal()
 
-foo <- qvalue(jDat$rtProbsObs)
-foo$pi0
-plot(foo)
+dev.print(pdf,
+          "../figs/scatterplot-qValues-vs-pValues.pdf",
+          width = 8, height = 6.5)
 
-# dev.print(pdf,
-#           jPaste(whereAmI, "figs/qvaluePlot-rtProbsObs.pdf"),
-#           width = 8, height = 8)
+## if we threshholded based on q-values from the "observed" test statistics, is
+## the empirical FDR less than or equal to the threshhold?
+realityCheck <-
+  ddply(droplevels(subset(pDat, source == "tObs")),
+        ~ test, function(x) {
+          x <- arrange(x, qVal)
+          x$nD <- seq_len(nrow(x))
+          x$nFD <- cumsum(x$isNull)
+          x$fdr <- with(x, nFD / nD)
+          x
+        })
+str(realityCheck)
 
-addmargins(table(foo$qvalues < 0.40, jDat$stateOfNature), 2)
-17/30
+p <- ggplot(realityCheck, aes(x = qVal, y = fdr)) +
+  xlab("q-value cutoff = putative FDR") + ylab("achieved FDR")
 
-max(jDat$rtProbsObs[foo$qvalues < 0.40])
+p + facet_wrap(~ test) + coord_equal(xlim = 0:1, ylim = 0:1) +
+  geom_abline(intercept = 0, slope = 1, col = "orange", lwd = 1.5) +
+  geom_point()
 
-xyplot(foo$qvalues ~ rtProbsObs | stateOfNature, jDat,
-       groups = yo, auto.key = TRUE)
-##Error in eval(expr, envir, enclos) : object 'yo' not found
+dev.print(pdf,
+          "../figs/achieved-fdr-vs-putative-fdr.pdf",
+           width = 8, height = 8)
 
-# dev.print(pdf,
-#           jPaste(whereAmI, "figs/qvalue-vs-pvalue--rtProbsObs.pdf"),
-#           width = 8, height = 8)
-
-densityplot(~ foo$qvalues | stateOfNature, jDat)
-
-foo <- qvalue(jDat$tsProbsNull)
-foo$pi0
-plot(foo)
-
-foo <- qvalue(jDat$tsProbsExc)
-foo$pi0
-plot(foo)
-
-foo <- qvalue(jDat$tsProbsObs)
-foo$pi0
-plot(foo)
-
-addmargins(table(foo$qvalues < 0.70, jDat$stateOfNature), 2)
-17/40
-
-max(jDat$rtProbsObs[foo$qvalues < 0.40])
-
-xyplot(foo$qvalues ~ tsProbsObs | stateOfNature, jDat,
-       groups = yo, auto.key = TRUE)
-## Error in eval(expr, envir, enclos) : object 'yo' not found
-
-densityplot(~ foo$qvalues | stateOfNature, jDat)
-
-
+## old code, using base graphics, to make the pretty pictures for depicting the
+## p-values; not worth redoing in ggplot2; keeping for posterity, though
 
 ## draw the null dist'n
 tstStat <- 1.3
